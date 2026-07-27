8f9311fc6632f1ba28767ebd882f934e3cfc9f8c
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-27 23:07:03 +0100

    Create Bash.zsh

---
+++ Bash.zsh
@@ -0,0 +1,4 @@
+export LANGSMITH_TRACING="true"
+export LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+export LANGSMITH_API_KEY="your_langsmith_api_key"
+export LANGSMITH_PROJECT="MyModelDeployment"
